0212ff947c6e369f634604577e5ca84b601c755c
Daniel Han <hex0cter@gmail.com>
2018-11-13 21:21:33 +0100

    Enable redux debugger

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const rootReducer = combineReducers({
     hist: historyReducer
 })
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
