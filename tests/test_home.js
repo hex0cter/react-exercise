5e030c295a83a7874be8d166d90b17cec71236e7
Daniel Han <daniel.han@scrive.com>
2020-07-15 14:28:45 +0200

    try 10 browsers

--- tests/test_home.js
+++ tests/test_home.js
@@ -1,27 +1,52 @@
 fixture `Getting Started`
     .page `http://devexpress.github.io/testcafe/example`;
 
-test('My first test', async t => {
+test('My 1 test', async t => {
     // Test code
     await t.navigateTo("https://danielhan.dev/")
 });
 
-test('My second test', async t => {
+test('My 2 test', async t => {
     // Test code
     await t.navigateTo("https://danielhan.dev/")
 });
 
-test('My third test', async t => {
+test('My 3 test', async t => {
     // Test code
     await t.navigateTo("https://danielhan.dev/")
 });
 
-test('My fourth test', async t => {
+test('My 4 test', async t => {
     // Test code
     await t.navigateTo("https://danielhan.dev/")
 });
 
-test('My fifth test', async t => {
+test('My 5 test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My 6 test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My 7 test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My 8 test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My 9 test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My 10 test', async t => {
     // Test code
     await t.navigateTo("https://danielhan.dev/")
 });
