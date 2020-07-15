6bd4a60b3e9e0175fa33769ea6494c50b17f0ac8
Daniel Han <daniel.han@scrive.com>
2020-07-15 14:19:33 +0200

    Parallize tests

--- tests/test_home.js
+++ tests/test_home.js
@@ -1,8 +1,27 @@
-import { Selector } from 'testcafe';
-
 fixture `Getting Started`
     .page `http://devexpress.github.io/testcafe/example`;
 
 test('My first test', async t => {
     // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My second test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My third test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My fourth test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
+});
+
+test('My fifth test', async t => {
+    // Test code
+    await t.navigateTo("https://danielhan.dev/")
 });
